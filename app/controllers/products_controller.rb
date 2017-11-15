ece6b8c5a85e867d80e7a46254706b9cc249c0d8
Patricia Morad <pjmorad@gmail.com>
2017-11-14 19:27:19 -0600

    Create route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,32 @@
 class ProductsController < ApplicationController
+
+  def pants_method
+    product = Product.first
+    render json: product.as_json
+  end 
+
+  def shirt_method 
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def sweatshirt_method
+    product = Product.third
+    render json: product.as_json
+  end 
+
+  def shoes_method
+    product = Product.fourth
+    render json: product.as_json
+  end
+
+  def handbag_method
+    product = Product.fifth 
+    render json: product.as_json 
+  end 
+
+  def all_products_method
+    product = Product.sixth
+    render json: product.as_json
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/pants_url" => "products#pants_method"
+  get "/shirt_url" => "products#shirt_method"
+  get "/sweatshirt_url" => "products#sweatshirt_method"
+  get "/shoes_url" => "products#shoes_method"
+  get "/handbag_url" => "products#handbag_method"
+  get "/all_products_url" => "products#all_products_method"
 end
+
+
